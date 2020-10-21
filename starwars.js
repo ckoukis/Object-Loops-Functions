8635f47ed3d811bde37b01804827b42eff82422a
Christos Koukis <ckoukis@Christoss-Mac-mini.local>
2020-10-21 13:37:06 -0600

    [app.js] Add object practice

---
+++ starwars.js
@@ -0,0 +1,12 @@
+const theMandolorian = {
+    image: "path to where image is located",
+    imageAge: "1 day old",
+    imageTitle: "Special Look | The Mandolorian | Disney+",
+    length: "60s",
+    views: "1M",
+    author: "Star Wars",
+    copyRight: "DisneyOriginal",
+    characters: ["Mando", "The Child"]
+}
+
+console.log(theMandolorian.characters)
